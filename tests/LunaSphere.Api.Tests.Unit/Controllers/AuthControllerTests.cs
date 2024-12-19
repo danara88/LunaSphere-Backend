e3e15a9f00772b7150574a8dffe1bbe194833169
Daniel Aranda <c-daniel.aranda@charter.com>
2024-12-19 15:33:17 -0600

    Cover unit tests for login functionality

--- tests/LunaSphere.Api.Tests.Unit/Controllers/AuthControllerTests.cs
+++ tests/LunaSphere.Api.Tests.Unit/Controllers/AuthControllerTests.cs
@@ -11,6 +11,7 @@ using LunaSphere.Application.Auth.DTOs;
 using LunaSphere.Application.Users.DTOs;
 using LunaSphere.Controllers;
 using LunaSphere.Domain.Users;
+using LunaSphere.Application.Auth.Commands.LoginCommand;
 
 namespace LunaSphere.Api.Tests.Unit.Controllers;
 
@@ -50,7 +51,6 @@ public class AuthControllerTests
         result.Value.Should().BeEquivalentTo(new ApiResponse<AuthDTO>(mockAuthDTO, HttpStatusCode.Created));
     }
     
-
     [Fact]
     public async Task Register_ShouldReturnConflictErrorWithExpectedMessage_WhenEmailAlreadyExists() {
         // Arrange
@@ -84,6 +84,52 @@ public class AuthControllerTests
         {
             Status =StatusCodes.Status400BadRequest,
             Detail = AuthErrors.WeakPassword.Description
+        });
+    }
+
+    [Fact]
+    public async Task Login_ShouldReturnOkObjectResult_WhenCredentialsAreValid()
+    {
+        // Arrange
+         var mockLoginUser = new LoginUserDTO(Email: "test@tests.com", Password: "Strong_Password!");
+         var mockAuthDTO = new AuthDTO
+        (
+            AccessToken: "xyztokenxyz",
+            UserDetails: new UserDTO
+            (
+                FirstName: null,
+                LastName: null,
+                Email: mockLoginUser.Email,
+                IsGoogle: false,
+                LastLogin: DateTime.UtcNow
+            )
+        );
+         _mediator.Send(Arg.Any<LoginCommand>()).Returns(mockAuthDTO);
+
+        // Act
+        var result = (ObjectResult) await _sut.Login(mockLoginUser);
+
+        // Assert
+        result.StatusCode.Should().Be(StatusCodes.Status200OK);
+        result.Value.Should().BeEquivalentTo(new ApiResponse<AuthDTO>(mockAuthDTO));
+    }
+
+    [Fact]
+    public async Task Login_ShouldReturnUnAuthorizedWithExpectedMessage_WhenCredentialsAreInvalid()
+    {
+        // Arrange
+        var mockLoginUserDTO = new LoginUserDTO(Email: "test@tests.com", Password: "123");
+        _mediator.Send(Arg.Any<LoginCommand>()).Returns(AuthErrors.InvalidCredentials);
+
+        // Act
+        var result = (ObjectResult) await _sut.Login(mockLoginUserDTO);
+
+        // Assert
+        result.StatusCode.Should().Be(StatusCodes.Status401Unauthorized);
+        result.Value.Should().BeEquivalentTo(new ProblemDetails 
+        {
+            Status =StatusCodes.Status401Unauthorized,
+            Detail = AuthErrors.InvalidCredentials.Description
         });
     }
 }
